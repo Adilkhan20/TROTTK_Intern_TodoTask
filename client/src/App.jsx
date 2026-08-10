fabfb089f4475459b0d2065b36a98bd4a72cfcf1
Adil Khan <adilkhansumbal7@gmail.com>
2026-08-10 21:50:43 +0500

    Implement backend authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
+import { useContext } from "react";
+import Register from "./Components/Register";
 import Login from "./Components/Login";
+import { TodoContext } from "./store/ContextApi";
 
 function App() {
-  return (
-    <>
-      <Login></Login>
-    </>
-  );
+  const { isLoggedIn } = useContext(TodoContext);
+  console.log(isLoggedIn);
+
+  return <>{isLoggedIn ? <Login /> : <Register />}</>;
 }
 
 export default App;
---
+++ client/src/utils/constant.js
@@ -0,0 +1,4 @@
+export const HOST = "http://localhost:3000";
+
+export const LoginRoute = `${HOST}/api/login`;
+export const RegisterRoute = `${HOST}/api/register`;
