3489829818dc5682000126e4309fdf66cba0cf69
Adil Khan <adilkhansumbal7@gmail.com>
2026-08-11 15:16:21 +0500

    Context issue resolved

--- client/src/store/ContextApi.jsx
+++ client/src/store/ContextApi.jsx
@@ -1,13 +1,18 @@
 import { createContext, useState } from "react";
 
-export const TodoContext = createContext();
+const TodoContext = createContext();
 
 export const TodoListProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentScreen, setCurrentScreen] = useState("register");
 
   return (
-    <TodoContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+    <TodoContext.Provider
+      value={{ isLoggedIn, setIsLoggedIn, currentScreen, setCurrentScreen }}
+    >
       {children}
     </TodoContext.Provider>
   );
 };
+
+export default TodoListProvider;
